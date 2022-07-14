2a00f2dd7cdc35d788aa5b1c70b928c981201ba6
Georgian Camarasan <georgian.camarasan@zohomail.eu>
2022-07-14 13:19:52 +0300

    Updated project patterns

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -5,10 +5,11 @@ end
 project.setup({
 
 	-- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
-	detection_methods = { "pattern" },
+	detection_methods = { "lsp", "pattern" },
 
 	-- patterns used to detect root dir, when **"pattern"** is in detection_methods
-	patterns = { ".git", "Makefile", "package.json" },
+	patterns = { ".git", "Makefile", "package.json", "*.sln", "Cargo.toml" },
+  silent_chdir = false,
 })
 
 local tele_status_ok, telescope = pcall(require, "telescope")
